5c836623ee340f9b65370023e070048ed34bc41d
ilMatadore <restarter.dev@gmail.com>
2021-06-18 14:07:33 -0500

    fixing env2

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-api:travel123@nasacluster.3ygj2.mongodb.net/nasa?retryWrites=true&w=majority'
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!');
